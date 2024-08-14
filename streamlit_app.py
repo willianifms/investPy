51568c1c34f88f8df0b19d044ee353530c1c1d82
CaioHPerlin <caiohperlin@gmail.com>
2024-08-14 16:07:36 -0400

    feat: implement graphics

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,55 +1,14 @@
 import streamlit as st
 
-# `st.title(" My new app")` is a Streamlit function that sets the title of the web application to "My
-# new app". This function is used to display a title at the top of the web page to provide a heading
-# or a name for the application.
-st.title("📈 InvestPy")
-st.write(
-    "Simulador de Investimentos"
-)
+# Definição das páginas
+home_page = st.Page("./pages/home.py", title="O que é o InvestData?", icon="🏠")
+custom_page = st.Page("./pages/custom.py", title="Renda Fixa", icon="💰")
+piggybank_page = st.Page("./pages/piggybank.py", title="Cofrinhos", icon="🐷")
 
-    
+# Configuração da navegação entre páginas
+pg = st.navigation({
+    "Bem-vindo (a)": [home_page],
+    "Simuladores": [custom_page, piggybank_page]
+})
 
-
-
-option = st.selectbox(
-    "Qual Será o tipo de investimento?",
-    ("🏛 Cofrinho do Inter", "🏛 Cofrinho do Nubank", "🏛 Cofrinho do PicPay"))
-
-st.write("Investimento selecionado:", option)
-
-genre = st.radio(
-    "Aporte inicial",
-    ["Sim", "Não"],
-    index=None,
-)
-
-if genre == "Sim":
-    st.write("Digite o valor do aporte inicial")
-    aporte = st.number_input('Valor do aporte', value=0)
-    st.write("Aporte inicial:", aporte)
-    
-mensal = st.radio(
-    "Investimento Mensal",
-    ["Sim", "Não"],
-    index=None,
-)
-
-if mensal == "Sim":
-    st.write("Digite o valor do aporte mensal")
-    aporte_mensal = st.number_input('Valor do aporte mensal')
-    st.write("Aporte mensal:", aporte_mensal)
-
-
-tempoInvestimento = st.selectbox(
-    "Qual Será o tipo de investimento?",
-    ("1 ano", "3 anos", "5 anos", "10 anos", "20 anos", "30 anos"))
-
-st.write("Investimento selecionado:", tempoInvestimento)
-
-if option == "🏛 Cofrinho do Inter" and aporte_mensal >=1 or aporte >=1:
-    st.title("📈 Resultado do investimento")
-    resultAporte = aporte + (aporte ** 0.081)
-    resultMensal = aporte_mensal + (aporte_mensal ** 0.081)
-    result = resultAporte + resultMensal
-    st.write("Aporte mensal:", result)
+pg.run()
